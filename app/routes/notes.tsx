e20199517c9a19e2e5c53a8854dce1e65e813c78
kennedy-muhumuza <muhumuzakennedy17@gmail.com>
2024-10-17 00:17:20 +0300

    update notes action function

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,7 +1,9 @@
+// import { redirect } from "@remix-run/react";
+import { redirect } from "@remix-run/react";
 import React from "react";
 import NewNote, { links as newNoteLinks } from "~/components/NewNote";
 import newNoteStyles from "~/components/NewNote.css";
-import { getStoredNotes } from "~/data/notes";
+import { getStoredNotes, storedNotes } from "~/data/notes";
 
 const NotesPage = () => {
   return (
@@ -20,6 +22,8 @@ export async function action({ request }) {
   const existingNotes = await getStoredNotes();
   noteData.id = new Date().toISOString();
   const updatedNotes = existingNotes.concat(noteData);
+  await storedNotes(updatedNotes);
+  return redirect("/notes");
 }
 
 export function links() {

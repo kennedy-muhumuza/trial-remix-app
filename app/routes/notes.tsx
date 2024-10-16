0bcf0a329f5dd6c26055db5f6941a15b8e20c133
kennedy-muhumuza <muhumuzakennedy17@gmail.com>
2024-10-16 23:54:39 +0300

    setup action for handling notes data submission

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NewNote, { links as newNoteLinks } from "~/components/NewNote";
 import newNoteStyles from "~/components/NewNote.css";
+import { getStoredNotes } from "~/data/notes";
 
 const NotesPage = () => {
   return (
@@ -11,6 +12,15 @@ const NotesPage = () => {
 };
 
 export default NotesPage;
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const noteData = Object.fromEntries(formData);
+  // Add Validation...
+  const existingNotes = await getStoredNotes();
+  noteData.id = new Date().toISOString();
+  const updatedNotes = existingNotes.concat(noteData);
+}
 
 export function links() {
   return [...newNoteLinks()];
